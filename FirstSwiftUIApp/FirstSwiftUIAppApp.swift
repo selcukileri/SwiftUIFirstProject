bc123ffd7b8c106c367e7cb2d0a074c2a99536d7
Selçuk İleri <selcukileri98@gmail.com>
2023-11-04 12:36:18 +0300

    Initial Commit

---
+++ FirstSwiftUIApp/FirstSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIAppApp.swift
+//  FirstSwiftUIApp
+//
+//  Created by Selçuk İleri on 4.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
